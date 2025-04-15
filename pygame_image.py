9546cb741ea77c2d928f34961ee7d45d73169070
Okamoto <c0a2405812@edu.teu.ac.jp>
2025-04-15 15:36:12 +0900

    練習5_FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -19,11 +19,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300,200])
+        screen.blit(bg_img, [0, 0]) #ren3
+        screen.blit(kk_img, [300,200]) #ren4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200) #ren5
 
 
 if __name__ == "__main__":
