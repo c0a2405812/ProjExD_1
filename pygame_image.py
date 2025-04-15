796a77eba79a066196a9deab2cb5fdf8d06cad85
Okamoto <c0a2405812@edu.teu.ac.jp>
2025-04-15 16:45:04 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -17,6 +17,7 @@ def main():
     
     kk_rct = kk_img.get_rect() #ren10　自分で移動させるこうかとん
     kk_rct.center = 300, 200
+
     
     
     tmr = 0
@@ -24,6 +25,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        X = 0
+        Y = 0
         x = tmr%3200 #ren6
         screen.blit(bg_img, [-x, 0]) #ren3  #ren6
         screen.blit(bg_img2, [-x+1600,0]) #ren7
@@ -31,17 +34,18 @@ def main():
 
         key_lst = pg.key.get_pressed() #ren10 (True or Falseを出力)
         if key_lst[pg.K_UP]: #リストの中で上キーが押されているならmove_ipする
-            kk_rct.move_ip((0, -1))
+            Y = -1
         elif key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
+            Y = 1
         elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            X = 1
         elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            X = -1
         
         if key_lst[pg.K_RIGHT] == False:
-            kk_rct.move_ip((-1,0))
+            X = -1
 
+        kk_rct.move_ip((X,Y))
         screen.blit(kk_img, kk_rct) #ren4
         pg.display.update()
         tmr += 1        
